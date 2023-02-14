bb1da2c6b02ea5a2aefad0749ef9367a80e000b4
gaurav.taple <taplegaurav1234@gmail.com>
2023-02-14 17:45:05 +0530

    commit

---
+++ src/com/example/hello_world/Demo.java
@@ -0,0 +1,12 @@
+package com.example.hello_world;
+
+public class Demo {
+	public String Calling()
+	{
+		System.out.println("Hello world");
+		return "Hello";
+		
+	}
+	
+
+}
---
+++ src/com/example/hello_world/Demo1.java
@@ -0,0 +1,10 @@
+package com.example.hello_world;
+
+public class Demo1 extends Demo {
+	public static void main(String[] args) {
+		Demo1 d1=new Demo1();
+		d1.Calling();
+	}
+	
+
+}
